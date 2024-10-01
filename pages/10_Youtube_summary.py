afd103c5a1d641cfdd8b9d137aa3b11851c43f5f
Nicola Masseroni <n.masseroni@fabbricadigitale.it>
2024-10-01 15:20:19 +0000

    feat: Add .gitignore file and update requirements.txt
    Add .gitignore file to ignore unnecessary files and directories.
    Update requirements.txt to include langchain-openai package.

--- pages/10_Youtube_summary.py
+++ pages/10_Youtube_summary.py
@@ -1,11 +1,12 @@
 import streamlit as st
-from langchain.chains.combine_documents.stuff import StuffDocumentsChain
-from langchain.chains.llm import LLMChain
+#from langchain.chains.combine_documents.stuff import StuffDocumentsChain
+#from langchain.chains.llm import LLMChain
 from langchain.prompts import PromptTemplate
 from langchain.chains.summarize import load_summarize_chain
-from langchain_anthropic import ChatAnthropic
+#from langchain_anthropic import ChatAnthropic
+from langchain_openai import ChatOpenAI
 from langchain_community.document_loaders import YoutubeLoader
-
+from langchain.chains.combine_documents import create_stuff_documents_chain
 
 def prompt_summary(url):
     loader = YoutubeLoader.from_youtube_url(
@@ -14,42 +15,53 @@ def prompt_summary(url):
         language=["it", "en", "fr", "de"]
     )
 
-    loader.load()
+#    loader.load()
+    docs = loader.load()
 
     # Define prompt
     prompt_template = """This is a transcript of a Youtube video.
     Please, write a summary of the themes of th video using the same language of the transcript.
 
     TRANSCRIPT:
-    "{text}"
+    "{context}"
 
     DETAILED SUMMARY:"""
+
     prompt_template = """This is a transcript of a Youtube video.
     Please identify the themes that may be unique to this video and write a detailed and comprehensive summary of the video.
     Use the ITALIAN language to write the summary.
 
     TRANSCRIPT:
-    "{text}"
+    "{context}"
 
     DETAILED SUMMARY AND HIGHLIGHTS:"""
+
+    prompt_template = """Below is a transcript of a YouTube video. Please analyze the content and identify any themes or topics 
+    that may be unique to this specific video. Based on your analysis, write a detailed and comprehensive summary in Italian, 
+    capturing the key points and nuances of the discussion.
+
+    TRANSCRIPT:
+    "{context}"
+
+    DETAILED SUMMARY AND HIGHLIGHTS:"""
+
 
     prompt = PromptTemplate.from_template(prompt_template)
 
     # Define LLM chain
-    #    llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo-16k")
-    llm = ChatAnthropic(model=MODEL, temperature=0)
+    llm = ChatOpenAI(temperature=0, model_name="gpt-4o-mini-2024-07-18")
+    #llm = ChatAnthropic(model=MODEL, temperature=0)
+    chain = create_stuff_documents_chain(llm, prompt)
 
-    llm_chain = LLMChain(llm=llm, prompt=prompt)
-
-    # Define StuffDocumentsChain
-    stuff_chain = StuffDocumentsChain(
-        llm_chain=llm_chain, document_variable_name="text"
-    )
-
-    docs = loader.load()
+#    llm_chain = LLMChain(llm=llm, prompt=prompt)
+    # stuff_chain = StuffDocumentsChain(
+    #     llm_chain=llm_chain, document_variable_name="text"
+    # )
     #    print(docs)
-    resp = stuff_chain.invoke(docs)
-    return resp["output_text"]
+#    resp = stuff_chain.invoke(docs)
+    resp = chain.invoke({"context": docs})
+#    print(resp)
+    return resp
 
 
 # def standard_summary(url):
